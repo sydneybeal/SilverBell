a3d505a141550461d0487934d83856d3207aed46
Sydney Beal <sydneybeal@miami.edu>
2018-03-26 16:23:21 -0400

    Added caretaker side of app

---
+++ SilverBell/SilverBell/ViewControllers/CaretakerHomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  CaretakerHomeVC.swift
+//  SilverBell
+//
+//  Created by Sydney Beal on 3/15/18.
+//  Copyright © 2018 Jackson Rossborough. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+class HomeVC: UIViewController {
+    
+    
+    
+    
+}
