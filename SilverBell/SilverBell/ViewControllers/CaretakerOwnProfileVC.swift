92e1fd52c3c587b34c90aed342668dc246c069d2
Sydney Beal <sydneybeal@miami.edu>
2018-04-06 19:17:46 -0400

    Improved caretakerNavVC functionality, still not perfect

---
+++ SilverBell/SilverBell/ViewControllers/CaretakerOwnProfileVC.swift
@@ -0,0 +1,103 @@
+//
+//  ProfileVC.swift
+//  SilverBell
+//
+//  Created by Jackson Rossborough on 1/29/18.
+//  Copyright © 2018 Jackson Rossborough. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import Photos
+
+class CaretakerOwnProfileVC: UIViewController {
+    
+    @IBOutlet weak var profilePicView: RoundedImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    let imagePicker = UIImagePickerController()
+    
+    func customization() {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.fetchUserInfo()
+    }
+    
+    //Downloads current user credentials
+    func fetchUserInfo() {
+        if let id = Auth.auth().currentUser?.uid {
+            User.info(forUserID: id, completion: {[weak weakSelf = self] (user) in
+                DispatchQueue.main.async {
+                    weakSelf?.nameLabel.text = user.name
+                    weakSelf?.emailLabel.text = user.email
+                    weakSelf?.profilePicView.image = user.profilePic
+                    weakSelf = nil
+                }
+            })
+        }
+        else {print("Did not auth")}
+    }
+    
+    @IBAction func logOutUser(_ sender: Any) {
+        User.logOutUser { (status) in
+            if status == true {
+                weak var pvc = self.presentingViewController
+                self.dismiss(animated: true){
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Welcome") as! WelcomeVC
+                    pvc?.present(vc, animated: true)
+                }
+            }
+        }
+    }
+    
+    func openPhotoPickerWith(source: PhotoSource) {
+        switch source {
+        case .camera:
+            let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+            if (status == .authorized || status == .notDetermined) {
+                self.imagePicker.sourceType = .camera
+                self.imagePicker.allowsEditing = true
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+        case .library:
+            let status = PHPhotoLibrary.authorizationStatus()
+            if (status == .authorized || status == .notDetermined) {
+                self.imagePicker.sourceType = .savedPhotosAlbum
+                self.imagePicker.allowsEditing = true
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func selectPic(_ sender: Any) {
+        let sheet = UIAlertController(title: nil, message: "Select the source", preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.openPhotoPickerWith(source: .camera)
+        })
+        let photoAction = UIAlertAction(title: "Gallery", style: .default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.openPhotoPickerWith(source: .library)
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        sheet.addAction(cameraAction)
+        sheet.addAction(photoAction)
+        sheet.addAction(cancelAction)
+        self.present(sheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.profilePicView.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.customization()
+    }
+    
+    
+}
+
